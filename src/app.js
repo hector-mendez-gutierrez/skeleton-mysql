53a405abdf03a013180d61355d9f8faf38d91d0b
Hector Mendez <hector.mendez@tecnosecurity.net>
2022-04-23 21:51:45 -0400

    Estructura base

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const { urlencoded } = require("express");
+const express = require("express");
+
+const app = express();
+
+//Settings
+
+app.use(express.json());
+
+//Midleware
+
+//Rutas
+
+module.exports = app;
